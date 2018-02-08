4681426726eff60ed79e0060e58022f31d29a995
ashlynnpai <ashlynnpai@gmail.com>
2018-02-08 17:02:12 -0500

    clean up comments

--- gol.py
+++ gol.py
@@ -1,42 +1,28 @@
-cells = [[1,0,0],
-         [0,1,1],
-         [1,1,0]]
-
-generations = 1
-
-# got total counts working
-    #handle index error, handle -1 problem
-
-# put totals into a dict
-
-#update the board
-
-#if 2 or 3 neighbors and cell is alive, keeps living
-#no change in state, do nothing
-#if 3 neighbors and cell is dead, comes alive
-
-#cell dies if less than 2 or greater than 3 neighbors
-
 
 def get_generation(cells, generations):
     tracker = {}
     for i, row in enumerate(cells):
         for j, num in enumerate(row):
             total = 0
+            #find 8 neighbors
             rows = [i-1, i, i+1]
             columns = [j-1, j, j+1]
             for r in rows:
                 for c in columns:
+                    #keep within the board and don't count the original
                     try:
                         if r != -1 and c != -1:
                             if r == i and c == j:
-                                total += 0
+                                pass
                             elif cells[r][c] == 1:
                                 total += 1
                     except IndexError:
                         pass
             k = str(i) + str(j)
+            #store cell values and neighbor totals in a dict using "ij" as keys
+            #{'00': [1, 1], '01': [0, 3]}
             tracker[k] = [cells[i][j], total]
+    #update cell values
     for arr in tracker.values():
         if arr[1] == 3:
             arr[0] = 1
