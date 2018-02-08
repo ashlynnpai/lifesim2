3b3cf8d711223a069ddfd652965e9ae87fc0aac7
ashlynnpai <ashlynnpai@gmail.com>
2018-02-08 15:06:07 -0500

    add tracker

--- gol.py
+++ gol.py
@@ -4,8 +4,14 @@ cells = [[1,0,0],
 
 generations = 1
 
+#current state
+#neighbor current
+#final state
+#tracker = {'00': [1]}
+
 
 def get_generation(cells, generations):
+    tracker = {}
     for i, row in enumerate(cells):
         for j, num in enumerate(row):
             total = 0
@@ -22,13 +28,23 @@ def get_generation(cells, generations):
 
                     except:
                         print "oor"
-            print "i" + str(i)
-            print "j" + str(j)
-            print total
-            print "---"
+
+            k = str(i) + str(j)
+            tracker[k] = [cells[i][j], total]
+    print tracker
 
 # got total counts working
+    #handle index error, handle -1 problem
+#try to go for list comprehension before the logic is worked out
 
 # put totals into a dict
 
 #update the board
+    updates = {}
+    for item in tracker:
+        pass
+        #if 2 or 3 neighbors and cell is alive, keeps living
+        #no change in state, do nothing
+        #if 3 neighbors and cell is dead, comes alive
+
+        #cell dies if less than 2 or greater than 3 neighbors
